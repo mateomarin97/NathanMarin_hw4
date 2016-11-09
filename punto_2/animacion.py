15fe45a0bce9c77dda9acb9d37b9f01cf06d3fa0
Nathan <mateomarin97@hotmail.com>
2016-11-08 22:59:39 -0500

    Version final

---
+++ punto_2/animacion.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+os.makedirs("./graficas")
+x=np.linspace(0,100,1000)
+datos= np.loadtxt("datos.dat")
+print(len(x))
+print(len(datos[0,:]))
+for i in range(len(datos[:,0])):
+    plt.figure(figsize=(10,10))
+    plt.plot(x,datos[i,:])
+    plt.xlim((0,100))
+    plt.ylim((-2,2))
+    plt.savefig("./graficas/"+str(i)+".jpg")
+    plt.close()
+
+os.system("sudo apt-get install imagemagick")
+os.chdir("./graficas")
+os.system("convert -delay 20 -loop 0 *.jpg cuerda.gif")
+os.system("mv cuerda.gif ..")
+os.chdir("..")
+os.system("rm -rf graficas")
